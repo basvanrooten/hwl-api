fd5042d40cddc95ac167416a5b0af924e8ff7d30
Bas van Rooten <basvanrooten@me.com>
2019-07-26 15:13:36 +0200

    Added Communication Test
    Added test to verify internet connectivity.

--- controllers/test_controller.js
+++ controllers/test_controller.js
@@ -1,12 +1,24 @@
 const ApiResponse = require('../models/ApiResponse');
 var logger = require('tracer').colorConsole();
+const axios = require('axios');
 
 
 module.exports = {
 
-    test(req, res, next) {
+    responseTest(req, res, next) {
 
        res.status(200).send(new ApiResponse("Acknowledged", 200));
        logger.debug("Returned test message");
+    },
+
+    communicationTest(req, res, next) {
+        axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+            .then(result => {
+                logger.debug(result.data);
+                res.status(200).send(result.data);
+            }).catch(e => {
+                logger.error("Can't connect to NASA API, possible internet outtage? ERROR: ", e.message);
+                res.status(503).send(new ApiResponse(e.message, 503));
+            })
     }
 }
--- routes/test_routes.js
+++ routes/test_routes.js
@@ -1,6 +1,8 @@
 let routes = require('express').Router();
 let TestController = require('../controllers/test_controller');
 
-routes.get('/test', TestController.test);
+// Test Routes
+routes.get('/test', TestController.responseTest);
+routes.get('/test/communication', TestController.communicationTest);
 
 module.exports = routes;
